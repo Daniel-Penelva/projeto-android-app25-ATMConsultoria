673bb30937a8346041b202d38b0dfe11d5b7bbbe
Daniel Penelva <d4n.andrade@gmail.com>
2023-05-06 02:41:07 -0300

    Abrindo uma ligação - usando Intent e ACTION_DIAL

--- app/src/main/java/com/example/atmconsultoria/MainActivity.java
+++ app/src/main/java/com/example/atmconsultoria/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.atmconsultoria;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -35,8 +37,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                enviarEmail();
             }
         });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -50,6 +51,12 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    public void enviarEmail(){
+
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:21995265588"));
+        startActivity(intent);
     }
 
     @Override
@@ -80,4 +87,76 @@ public class MainActivity extends AppCompatActivity {
 *     no mobile_navigation.xml.
 *
 * 8 - Configurar todos os ids no 'AppBarConfiguration.Builder()' em MainActivity.xml.
+*
+* ---------------------------------------------
+* Intents e filtros de intents
+*
+* O Intent é um objeto de mensagem que pode ser usado para solicitar uma ação de outro componente do aplicativo.
+* Embora os intents facilitem a comunicação entre os componentes de diversas formas, há três casos fundamentais de uso:
+*
+*   -> Como iniciar uma Activity (atividade)
+*      Uma Activity representa uma única tela em um aplicativo. É possível iniciar uma nova instância de uma Activity
+*      passando um Intent para startActivity(). O Intent descreve a atividade a iniciar e carrega todos os dados necessários.
+*      Se você quiser receber um resultado da atividade quando ela finalizar, chame startActivityForResult().
+*      Sua atividade recebe o resultado como um objeto Intent separado no callback de onActivityResult() da atividade.
+*
+*   -> Como iniciar um serviço
+*      O Service é um componente que realiza operações em segundo plano sem interface do usuário. Com o Android 5.0 (API nível 21)
+*      e posteriores, é possível iniciar um serviço JobScheduler.
+*      Para versões anteriores ao Android 5.0 (API nível 21), é possível iniciar um service usando os métodos da classe
+*      Service. É possível iniciar um serviço para realizar uma operação que acontece uma vez (como fazer o download
+*      de um arquivo) passando um Intent a startService(). O Intent descreve o serviço a iniciar e carrega todos os dados
+*      necessários. Se o serviço for projetado com uma interface servidor-cliente, é possível vincular ao serviço em
+*      outro componente passando um Intent a bindService(). Para mais informações, consulte o guia Serviços.
+*
+*   -> Como fornecer uma transmissão
+*      Transmissão é uma mensagem que qualquer aplicativo pode receber. O sistema fornece diversas transmissões para
+*      eventos do sistema, como quando o sistema inicializa ou o dispositivo inicia o carregamento. Você pode fornecer
+*      uma transmissão a outros aplicativos passando um Intent ao sendBroadcast() ou ao sendOrderedBroadcast().
+*
+* Tipos de intents
+* Há dois tipos de intents:
+*
+*  => Os intents explícitos especificam qual aplicativo atenderá ao intent, fornecendo o nome do pacote do aplicativo
+*     de destino ou o nome da classe de um componente totalmente qualificado. Normalmente, usa-se um intent explícito
+*     para iniciar um componente no próprio aplicativo porque se sabe o nome de classe da atividade ou do serviço que
+*     se quer iniciar. Por exemplo, iniciar uma nova atividade em resposta a uma ação do usuário ou iniciar um serviço
+*     para fazer o download de um arquivo em segundo plano.
+*
+*  => Os intents implícitos não nomeiam nenhum componente específico, mas declaram uma ação geral a realizar, o que
+*     permite que um componente de outro aplicativo a processe. Por exemplo, se você quiser exibir ao usuário uma
+*     localização em um mapa, pode usar um intent implícito para solicitar que outro aplicativo capaz exiba uma
+*     localização especificada no mapa.
+*
+*
+*                 intent           intent
+*                 |    |           |    |
+*                 |    |           |    |
+*     startActivity()  |           |   onCreate()
+*                 |    |           |    |
+*                 |    v           |    v
+*        Activity A   Android System   Activity B
+*             1             2              3
+*
+* OBS. Ilustração acima de como um intent implícito é fornecido pelo sistema para iniciar outra atividade:
+*      [1] A activity (atividade) A cria um Intent com uma descrição de ação e a passa para startActivity().
+*      [2] O sistema Android busca, em todos os aplicativos, um filtro de intents que corresponda ao intent. Ao encontrar
+*      uma correspondência, [3] o sistema inicia a atividade correspondente (Activity B) chamando seu método onCreate() e
+*      passando-lhe o Intent.
+*
+* A ilustração acima mostra como um intent é usado ao iniciar uma atividade. Quando o objeto Intent nomeia um componente
+* específico da atividade de forma explícita, o sistema inicia o componente de imediato.
+*
+* Ao criar um intent implícito, o sistema Android encontra o componente adequado para iniciar, comparando o conteúdo do
+* intent aos filtros de intents declarados no arquivo de manifest de outros aplicativos no dispositivo. Se o intent
+* corresponder a um filtro de intents, o sistema iniciará esse componente e entregará o objeto Intent. Se diversos filtros
+* de intents corresponderem, o sistema exibirá uma caixa de diálogo para que o usuário selecione o aplicativo que deseja
+* usar.
+*
+* O filtro de intents é uma expressão em um arquivo de manifest do aplicativo que especifica o tipo de intents que
+* o componente gostaria de receber. Por exemplo, ao declarar um filtro de intents para uma atividade, outros aplicativos
+* se tornam capazes de iniciar diretamente sua atividade com o determinado tipo de intent. Do mesmo modo, se você não
+* declarar nenhum filtro de intents para uma atividade, ela poderá ser iniciada somente com um intent explícito.
+*
+* Fonte: https://developer.android.com/guide/components/intents-filters?hl=pt-
 * */
